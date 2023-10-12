a485e0d0f240f9f76e1d283c6f76520a81a00ef0
PankajTete02 <pankajt0201@gmail.com>
2023-10-12 17:48:46 +0530

    main

--- app/controllers/api/v1/look_controller.rb
+++ app/controllers/api/v1/look_controller.rb
@@ -1,30 +1,14 @@
-# class Api::V1::LookController < ApplicationController
-#   def index
-#     p_parent_code = params[:P_PARENT_CODE]
-#     p_type = params[:P_TYPE]
 
-#     step2_service = LookUpService.new(p_parent_code, p_type)
-#     result = step2_service.step_2
-   
-#     render json: { message: result}
-    
-#   end
-# end
 
-# app/controllers/api/v1/look_controller.rb
+class Api::V1::LookController < ApplicationController
+    def index
+      p_parent_code = params[:P_PARENT_CODE]
+      p_type = params[:P_TYPE]
 
-# class Api::V1::LookController < ApplicationController
-#   def index
-#     p_parent_code = params[:P_PARENT_CODE]
-#     p_type = params[:P_TYPE]
-
-#     step2_service = LookUpService.new(p_parent_code, p_type)
-#     result = step2_service.step_2
-
-#     if result
-#       render json: { lookup_key: result[:lookup_key], lookup_description: result[:lookup_description] }
-#     else
-#       render json: { error: 'Failed to fetch lookup data' }, status: :unprocessable_entity
-#     end
-#   end
-# end
+      look_up_service = LookUpService.new(p_parent_code, p_type)
+      result = look_up_service.step_2
+  
+      render json: { message: result }
+    end
+  end
+  
--- app/services/look_up_service.rb
+++ app/services/look_up_service.rb
@@ -1,60 +1,32 @@
-# # class LookUpService
-# #   def initialize(p_parent_code, p_type)
-# #     @p_parent_code = p_parent_code
-# #     @p_type = p_type
-# #   end
-
-# #   def step_2
-# #     binding.break;
-# #     result = ActiveRecord::Base.connection.execute("
-# #       CALL USP_KODIE_FETCH_LOOKUP_MASTER(
-# #         '#{@p_parent_code}',
-# #         '#{@p_type}',
-# #         @lookup_key,
-# #         @lookup_description
-# #       )
-# #     ")
-    
-# #     lookup_key = ActiveRecord::Base.connection.execute('SELECT @lookup_key').first['@lookup_key']
-# #     lookup_description = ActiveRecord::Base.connection.execute('SELECT @lookup_description').first['@lookup_description']
-
-# #     { lookup_key: lookup_key, lookup_description: lookup_description }
-# #   end
-# # end
-
-
-
-# class LookUpService
-#   def initialize(p_parent_code, p_type)
-#     @p_parent_code = p_parent_code
-#     @p_type = p_type
-#   end
-
-#   def step_2
-#     result = ActiveRecord::Base.connection.execute(fetch_lookup_master_sql)
-#     parse_result(result)
-#   end
-
-#   private
-
-#   def fetch_lookup_master_sql
-#     if @p_type == 'QUESTION'
-#       "CALL USP_KODIE_FETCH_LOOKUP_MASTER('#{@p_parent_code}', '#{@p_type}', @p_lookup_key, @p_lookup_description);"
-#     else
-#       "CALL USP_KODIE_FETCH_LOOKUP_MASTER('#{@p_parent_code}', '#{@p_type}', @p_lookup_key, @p_lookup_description);"
-#     end
-#   end
-
-#   def parse_result(result)
-#     if result.present? && result.first.present?
-#       {
-#         lookup_key: result.first['P_LOOKUP_KEY'],
-#         lookup_description: result.first['P_LOOKUP_DESCRIPTION']
-#       }
-#     else
-#       # Handle the case where the result is empty or not as expected.
-#       nil
-#     end
-#   end
-# end
-
+class LookUpService
+    def initialize(parent_code, type)
+      @parent_code = parent_code
+      @type = type
+    end
+  
+    def step_2
+      sql = "CALL USP_KODIE_FETCH_LOOKUP_MASTER('#{@parent_code}', '#{@type}')"
+      result = ActiveRecord::Base.connection.raw_connection.query(sql)
+      
+      results = []
+      result.each do |row|
+        results << row
+      end
+      process_data(results)
+    end
+  
+    private
+  
+    def process_data(results)
+      
+      processed_data = results.map do |row|
+        {
+          lookup_key: row[0],
+          description: row[1]
+        }
+      end
+  
+      processed_data
+    end
+  end
+  

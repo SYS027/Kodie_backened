0ca6e1a340e0a9c9e286b5b73ba2c9d77e1cad7d
PankajTete02 <pankajt0201@gmail.com>
2023-10-24 20:50:09 +0530

    rreset

--- app/controllers/api/v1/reset_password_controller.rb
+++ app/controllers/api/v1/reset_password_controller.rb
@@ -3,9 +3,8 @@ class Api::V1::ResetPasswordController < ApplicationController
     def step_1_reset_password
         email = params[:email]
         session_service = ForgotPassword.new(email)
-        output = session_service.sp_reset_1(email)
-        # render json: { otp: result [0] , result: result ,status: true  }
-        render json: { email: email , result_0: output[0], result1:output[1],result: output,message: "Rails"}
+        result = session_service.sp_reset_1(email)
+        render json: {message: result[0]  ,generated_on: result[1] , status: true }
     end
 
     def step_2_reset_password

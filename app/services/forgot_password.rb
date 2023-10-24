f8a31d5d3c87f5207ef406aa7db807aa1b77fbd0
PankajTete02 <pankajt0201@gmail.com>
2023-10-24 17:38:13 +0530

    rreset

--- app/services/forgot_password.rb
+++ app/services/forgot_password.rb
@@ -1,32 +1,32 @@
 class ForgotPassword
-    def initialize(email)
-      @email = email
-    end
-  
-    def sp_reset_1(email)
-      begin
-        connection = ActiveRecord::Base.connection.raw_connection
-        @out_otp = 0
-        sql = "CALL RESET_PASSWORD_(?, @out_otp);"
-  
-        statement = connection.prepare(sql)
-        statement.execute(email)
-        statement.close
-  
-        query_select = "SELECT @out_otp AS otp;"
-        output_params = connection.query(query_select).first
-  
-        otp = output_params[0]
-        output_data = [otp]
-      rescue ActiveRecord::StatementInvalid => e
-        # Handle database statement execution error here
-        output_data =["#{e.message}"]
-      rescue StandardError => e
-        # Handle other exceptions here
-        output_data =["#{e.message}"]
-      ensure
-        # Ensure the database connection is closed
-        connection.close if connection
-      end
+  def initialize(email)
+    @email = email
+  end
+
+  def sp_reset_1(email)
+    begin
+      connection = ActiveRecord::Base.connection.raw_connection
+      @out_otp = 0
+      sql = "CALL RESET_PASSWORD_(?, @out_otp);"
+
+      statement = connection.prepare(sql)
+      statement.execute(email)
+      statement.close
+
+      query_select = "SELECT @out_otp AS otp;"
+      result = connection.query(query_select).first
+
+      otp = result[0]
+      output_data = [otp]
+    rescue ActiveRecord::StatementInvalid => e
+      # Handle database statement execution error here
+      output_data = ["#{e.message}"]
+    rescue StandardError => e
+      # Handle other exceptions here
+      output_data = ["#{e.message}"]
+    ensure
+      # Ensure the database connection is closed
+      connection.close if connection
     end
   end
+end

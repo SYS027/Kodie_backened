cc76ea01bd33fd70d8e607ef3be6c263962da509
PankajTete02 <pankajt0201@gmail.com>
2023-10-24 15:50:05 +0530

    reset

--- app/services/forgot_password.rb
+++ app/services/forgot_password.rb
@@ -1,28 +1,38 @@
 class ForgotPassword
+    def initialize(email)
+      @email = email
+    end
+  
    
-    # def initialize(email)
-    # @email = email 
-    # end
+    def sp_reset_1(email)
+      begin
+        connection = ActiveRecord::Base.connection.raw_connection
+        @out_otp = 0
+        sql = "CALL RESET_PASSWORD_(?, @out_otp);"
+  
+   
+  
+        statement = connection.prepare(sql)
+        statement.execute(email)
+        statement.close
+  
+   
+  
+        query_select = "SELECT @out_otp AS otp;"
+        output_params = connection.query(query_select).first
+  
 
-    # def sp_reset_1(email)
-    #     connection = ActiveRecord::Base.connection.raw_connection
-    #     @out_otp=0;
-    #     sql = "CALL RESET_PASSWORD_(?, @out_otp);"
-    
-    #     statement = connection.prepare(sql)
-    #     statement.execute(email)
-    #     statement.close
-       
-      
-    #     queryslect ="SELECT @out_otp AS otp;"
-    #     output_params = connection.query(queryslect).first
-        
-    #       otp = output_params[0],
-         
-        
-    #     output_data=[otp]
-       
-    # end
-
-
-end    
+        otp = output_params[0]
+        output_data = [otp]
+      rescue ActiveRecord::StatementInvalid => e
+        # Handle database statement execution error here
+        puts "Database statement error: #{e.message}"
+      rescue StandardError => e
+        # Handle other exceptions here
+        puts "An error occurred: #{e.message}"
+      ensure
+        # Ensure the database connection is closed
+        connection.close if connection
+      end
+    end
+  end

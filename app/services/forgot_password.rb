0bf20a1998dda66539a9584c9d17a997c784731b
PankajTete02 <pankajt0201@gmail.com>
2023-10-24 15:28:47 +0530

    reset pass

--- app/services/forgot_password.rb
+++ app/services/forgot_password.rb
@@ -1,28 +1,28 @@
 class ForgotPassword
    
-    def initialize(email)
-    @email = email 
-    end
+    # def initialize(email)
+    # @email = email 
+    # end
 
-    def sp_reset_1(email)
-        connection = ActiveRecord::Base.connection.raw_connection
-        @out_otp=0;
-        sql = "CALL RESET_PASSWORD_(?, @out_otp);"
+    # def sp_reset_1(email)
+    #     connection = ActiveRecord::Base.connection.raw_connection
+    #     @out_otp=0;
+    #     sql = "CALL RESET_PASSWORD_(?, @out_otp);"
     
-        statement = connection.prepare(sql)
-        statement.execute(email)
-        statement.close
+    #     statement = connection.prepare(sql)
+    #     statement.execute(email)
+    #     statement.close
        
       
-        queryslect ="SELECT @out_otp AS otp;"
-        output_params = connection.query(queryslect).first
+    #     queryslect ="SELECT @out_otp AS otp;"
+    #     output_params = connection.query(queryslect).first
         
-          otp = output_params[0],
+    #       otp = output_params[0],
          
         
-        output_data=[otp]
+    #     output_data=[otp]
        
-    end
+    # end
 
 
 end    

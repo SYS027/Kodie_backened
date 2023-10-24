f17db31ce778c6a533f11d8d0df68a8972c3a339
PankajTete02 <pankajt0201@gmail.com>
2023-10-24 16:45:14 +0530

    reset

--- app/services/forgot_password_Step_Two.rb
+++ app/services/forgot_password_Step_Two.rb
@@ -1,27 +1,11 @@
 class ForgotPasswordStepTwo
 
-    def initialize(email)
+    def initialize(email,otp)
         @email = email 
+        @otp =otp
     end
 
-    def sp_reset_1(email)
-        connection = ActiveRecord::Base.connection.raw_connection
-        @out_otp=0;
-        sql = "CALL RESET_PASSWORD_(?, @out_otp);"
-    
-        statement = connection.prepare(sql)
-        statement.execute(email)
-        statement.close
-       
-      
-        queryslect ="SELECT @out_otp AS otp;"
-        output_params = connection.query(queryslect).first
-        
-          otp = output_params[0]
-        
-        output_data=[otp]
-       
-    end
+   
      
     def sp_reset_2(email,otp)
         connection = ActiveRecord::Base.connection.raw_connection

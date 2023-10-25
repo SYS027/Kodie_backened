5d3314f0e820323664722a06df332b1b42e7e6d1
PankajTete02 <pankajt0201@gmail.com>
2023-10-25 15:15:00 +0530

    Kodie

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,6 @@
 class NotificationMailer < ApplicationMailer
-    default from: 'Pankaj.Tete@cylsys.com'
+    # default from: 'Pankaj.Tete@cylsys.com'
+    default from: "mailgun@https://app.mailgun.com/app/sending/domains/sandboxa35ea7c10b9440d9960d8fb0edab8ca9.mailgun.org"
     def alert_admin(otp)
     @otp=otp
      mail(to: params[:email],subject: "ALERT From ADMIN")

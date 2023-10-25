6969c29de1f2a12bdfc9014b5bf879e1d80685b2
PankajTete02 <pankajt0201@gmail.com>
2023-10-25 20:22:32 +0530

    Kodie

--- app/services/forgot_password.rb
+++ app/services/forgot_password.rb
@@ -35,6 +35,62 @@
 #     output_data
 #   end
 # end
+# class ForgotPassword
+#   def initialize(email)
+#     @email = email
+#   end
+
+#   def sp_reset_1(email)
+#     begin
+#       connection = ActiveRecord::Base.connection.raw_connection
+#       @out_otp = "000000"
+#       sql = "CALL RESET_PASSWORD_(?, @out_otp);"
+
+
+#       Rails.logger.error('step1')
+
+#       statement = connection.prepare(sql)
+#       Rails.logger.error('step2')
+#       statement.execute(email)
+#       Rails.logger.error('step3')
+      
+#       Rails.logger.error('step4')
+#       query_select = "SELECT @out_otp AS otp;"
+      
+#       connection.next_result if connection.respond_to?(:next_result)
+
+#       statement.close
+#       output_params = connection.query(query_select).first
+#       Rails.logger.error('step5')
+#       otp = output_params[0]
+#       Rails.logger.error(otp)
+      
+#       NotificationMailer.with(email: email).alert_admin(otp).deliver
+#       Rails.logger.error('step6')
+#       Rails.logger.error(otp.length())
+#       output_data =otp.to_s
+#       Rails.logger.error('step7')
+#       Rails.logger.error(output_data)
+     
+#     rescue ActiveRecord::StatementInvalid => e
+#       # Handle database statement execution error here
+#       puts "Database statement error: #{e.message}"
+#       Rails.logger.error("Database statement error: #{e.message}")
+#     rescue StandardError => e
+#       # Handle other exceptions here
+#       puts "An error occurred: #{e.message}"
+#       Rails.logger.error("An error occurred: #{e.message}")
+#       # NotificationMailer.alert_admin.deliver
+#     ensure
+#       output_data
+#       connection.close if connection
+#     end
+#   end
+# end
+
+
+
+
 class ForgotPassword
   def initialize(email)
     @email = email
@@ -45,7 +101,6 @@ class ForgotPassword
       connection = ActiveRecord::Base.connection.raw_connection
       @out_otp = "000000"
       sql = "CALL RESET_PASSWORD_(?, @out_otp);"
-
 
       Rails.logger.error('step1')
 
@@ -62,13 +117,13 @@ class ForgotPassword
       statement.close
       output_params = connection.query(query_select).first
       Rails.logger.error('step5')
-      otp = output_params[0][0][0][0][0][0]
+      otp = output_params[0].to_s
       Rails.logger.error(otp)
       
       NotificationMailer.with(email: email).alert_admin(otp).deliver
       Rails.logger.error('step6')
       Rails.logger.error(otp.length())
-      output_data =otp.to_s
+      output_data = otp.to_s
       Rails.logger.error('step7')
       Rails.logger.error(output_data)
      
@@ -87,9 +142,6 @@ class ForgotPassword
     end
   end
 end
-
-
-
 
 
 

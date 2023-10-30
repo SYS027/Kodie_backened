a205a5bfbc6b5b1356347803814b4a24af2b7e9c
PankajTete02 <pankajt0201@gmail.com>
2023-10-30 20:11:58 +0530

    Kodie

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,28 +1,30 @@
 class Api::V1::AuthController < ApplicationController
-
+ 
   def signup
-    
+   
     Rails.logger.error('Step1')
       email = params[:email]
       password = params[:password]
+      is_term_condition = params[:is_term_condition]
+      is_privacy_policy = params[:is_privacy_policy]
       otp = rand(100_000..999_999)
-      
-      
+     
+     
       Rails.logger.error('Step1')
-      signup_function_call = UspKodieSignupService.new(email, password,otp)
+      signup_function_call = UspKodieSignupService.new(email, password,is_term_condition,is_privacy_policy, otp)
       Rails.logger.error('Step1')
-      
+     
        result= signup_function_call.USP_KODIE_SIGNUP_DETAILS
        Rails.logger.error("result")
        Rails.logger.error(result)
-       if result == -1
+       if result === -1
         render json: { message: 'User Already Exists', status: false }
       else
         render json: { message: 'User Signup Successfull', User_Key: result, status: true }
       end
-  
+ 
   end
-
+ 
   def Signup_Check
     email = params[:email]
     otp = params[:otp]
@@ -30,17 +32,17 @@ class Api::V1::AuthController < ApplicationController
     result = session_service.sp_sign_up_otp_check(email, otp)
     Rails.logger.error("result")
     Rails.logger.error(result)
-    
+   
     if result == 1
       render json: { message: 'OTP Verified', status: true }
     else
       render json: { message: 'OTP Not Verified', status: false }
-    end 
-  end 
+    end
+  end
   private
-
+ 
   def generate_otp
     rand(100_000..999_999)
   end
-  
+ 
 end
--- app/services/usp_kodie_signup_service.rb
+++ app/services/usp_kodie_signup_service.rb
@@ -1,24 +1,36 @@
 class UspKodieSignupService
-  def initialize(email, password, otp)
+  def initialize(email, password, is_term_condition, is_privacy_policy, otp)
     @email = email
     @password = password
-    @otp = otp 
+    @is_term_condition = is_term_condition
+    @is_privacy_policy = is_privacy_policy
+    @otp = otp
   end
-
+ 
   def USP_KODIE_SIGNUP_DETAILS
     Rails.logger.error('Step1')
     Rails.logger.error(@otp)
-  
-    result = ActiveRecord::Base.connection.execute("SELECT UFUN_KODIE_SAVE_SIGNUP_DETAILS('#{@email}', '#{@password}', '#{@otp}') AS result")
+ 
+    statement = ActiveRecord::Base.connection.raw_connection.prepare(
+      "SELECT UFUN_KODIE_SAVE_SIGNUP_DETAILS(?, ?, ?, ?, ?) AS result"
+    )
+ 
+    result = statement.execute(
+      @email,
+      @password,
+      @is_term_condition,
+      @is_privacy_policy,
+      @otp
+    )
+ 
     NotificationMailer.with(email: @email).alert_admin(@otp).deliver
     Rails.logger.error('Step2')
-    Rails.logger.error(result.to_a) 
+    Rails.logger.error(result.to_a)
     value = result.to_a.flatten.first
     Rails.logger.error(value)
-   
+ 
+    statement.close
+ 
     value
   end
-
-  
 end
-

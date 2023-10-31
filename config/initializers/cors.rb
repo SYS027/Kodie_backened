dc1ce1489c3af3b3f3c06b6586c65b446d625d22
PankajTete02 <pankajt0201@gmail.com>
2023-10-31 17:33:28 +0530

    Swagger

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://127.0.0.1:3000"
+    origins "https://cylsys-kodie-api-01-e3fa986bbe83.herokuapp.com"
 
     resource "*",
       headers: :any,

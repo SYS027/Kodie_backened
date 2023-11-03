40084f44f671711e7b3a24c525dbb09cee7eb704
PankajTete02 <pankajt0201@gmail.com>
2023-11-03 20:09:39 +0530

    Kodie

--- app/controllers/api/v1/verification_controller.rb
+++ app/controllers/api/v1/verification_controller.rb
@@ -1,66 +1,88 @@
- class Api::V1::VerificationController < ApplicationController
+class Api::V1::VerificationController < ApplicationController
   require 'open-uri'
   require 'fileutils'
+   
+  def profile_photo
+    @profile_photo = params[:profile_photo]
+    content_type =  @profile_photo.content_type
+    @original_filename =  @profile_photo.original_filename
+    @temp_file_path =  @profile_photo.tempfile.path
+    Rails.logger.error("temp_file_path")
+    Rails.logger.error(@temp_file_path)
+    Rails.logger.error("temp_file_path2")
+    @first_name = params[:first_name]
+    @last_name = params[:last_name]
+    @physical_address = params[:physical_address]
+    @organisation_name = params[:organisation_name]
+    @referral_code = params[:referral_code]
+
+    Rails.logger.error(@profile_photo)
+    Rails.logger.error(@first_name)
+    Rails.logger.error(@last_name)
+    Rails.logger.error(@physical_address)
+    Rails.logger.error(@organisation_name)
+    Rails.logger.error(@referral_code)
+  end
+
   def account_details
-        {
-          user:  params[:user],
-          first_name: params[:first_name],
-          last_name: params[:last_name],
-          physical_address: params[:physical_address],
-          organisation_name: params[:organisation_name],
-          referral_code: params[:referral_code],
-          profile_photo: params[:profile_photo],
-    			describe_yourself: params[:describe_yourself],
-    			property_manage: params[:property_manage],
-    			kodie_help: params[:kodie_help]
-        }
-      end
-      def property_details
-        {
-         location: params[:location],
-         location_longitude: params[:location_longitude],
-         location_latitude: params[:location_latitude],
-         islocation: params[:islocation],
-         property_description: params[:property_description],
-         property_type: params[:property_type],
-         key_features: params[:key_features],
-         additional_features: params[:additional_features]
-    
-        }
-      end
- 
+    {
+      first_name: @first_name,
+      last_name: @last_name,
+      physical_address: @physical_address,
+      organisation_name: @organisation_name,
+      referral_code: @referral_code,
+      profile_photo: @original_filename
+    }
+  end
+
+  def property_details
+    {
+      location: @organisation_name,
+      location_longitude: @organisation_name
+      # Add other properties as needed
+    }
+  end
+
   def create
-    Rails.logger.error("1")
+    profile_photo
     Rails.logger.error("2")
- 
-    json_data = JSON.parse(request.raw_post)
-    account_details = json_data["account_details"]
-    property_details = json_data["property_details"]
- 
-    # Save the profile photo
-    profile_photo_url = account_details["profile_photo"]
-    if profile_photo_url
-      save_profile_photo(profile_photo_url)
+    Rails.logger.error(@first_name)
+    Rails.logger.error(@last_name)
+    
+    Rails.logger.error("2")
+    Rails.logger.error("2")
+
+    
+
+    profile_photo_path = params[:profile_photo]
+    if profile_photo_path.present?
+      save_profile_photo(profile_photo_path.tempfile)
+      account_details['profile_photo'] = profile_photo_path.original_filename
     end
- 
+
     result = UspKodieSaveStepsJsonData.new(account_details, property_details)
     result_data = result.create_post_with_tags
- 
+
     Rails.logger.error("result")
     Rails.logger.error(result_data)
- 
+
     render json: { message: "Data Successfully Stored", status: true }
   end
- 
+
   private
 
-  def save_profile_photo(url)
-    filename = File.basename(url)
+  def save_profile_photo(file)
+    filename = File.basename(file.path)
     local_path = Rails.root.join('public', 'images', filename)
-    Rails.logger.error(local_path)
-    # open(local_path, 'wb') do |file|
-    #   Rails.logger.error(file)
-    #   file << open(url).read
-    # end
+
+    FileUtils.cp(file.path, local_path)
+  end
+
+  def account_details_params
+    params[:account_details].present? ? JSON.parse(params[:account_details]) : {}
+  end
+
+  def property_details_params
+    params[:property_details].present? ? JSON.parse(params[:property_details]) : {}
   end
 end

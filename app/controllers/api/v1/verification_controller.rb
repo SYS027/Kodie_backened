34726a21d588bf1fc205f6c2f509e847ddf0be19
PankajTete02 <pankajt0201@gmail.com>
2023-11-15 12:13:38 +0530

    Kodie

--- app/controllers/api/v1/verification_controller.rb
+++ app/controllers/api/v1/verification_controller.rb
@@ -1,10 +1,149 @@
+# class Api::V1::VerificationController < ApplicationController
+#   require 'open-uri'
+#   require 'fileutils'
+   
+#   def profile_photo
+#     @profile_photo = params[:profile_photo]
+    
+#     Rails.logger.error("temp_file_path")
+#     Rails.logger.error(@temp_file_path)
+#     Rails.logger.error("temp_file_path2")
+#     @user = params[:user]
+#     @first_name = params[:first_name]
+#     @last_name = params[:last_name]
+#     @physical_address = params[:physical_address]
+#     @organisation_name = params[:organisation_name]
+#     @referral_code = params[:referral_code]
+#     @describe_yourself = params[:describe_yourself]
+#     @property_manage = params[:property_manage]
+#     @kodie_help = params[:kodie_help]
+#     @location =params[:location]
+#     @location_longitude = params[:location_longitude]
+#     @location_latitude =params[:location_latitude]
+#     @islocation =params[:islocation]
+#     @property_description =params[:property_description]
+#     @property_type =params[:property_type]
+#     @key_features =params[:key_features]
+#     @additional_features =params[:additional_features]
+#     Rails.logger.error(@profile_photo)
+#     Rails.logger.error(@first_name)
+#     Rails.logger.error(@last_name)
+#     Rails.logger.error(@physical_address)
+#     Rails.logger.error(@organisation_name)
+#     Rails.logger.error(@referral_code)
+#   end
+
+#   def account_details
+#     {
+#       user: @user,
+#       first_name: @first_name,
+#       last_name: @last_name,
+#       physical_address: @physical_address,
+#       organisation_name: @organisation_name,
+#       referral_code: @referral_code,
+#       profile_photo: @original_filename,
+#       describe_yourself: @describe_yourself,
+#       property_manage: @property_manage,
+#       kodie_help: @kodie_help
+
+#     }
+#   end
+
+#   def property_details
+#     {
+#       location: @organisation_name,
+#       location_longitude: @organisation_name,
+#       location_latitude: @location_longitude,
+#       islocation: @islocation,
+#       property_description: @property_description,
+#       property_type: @property_type,
+#       key_features: @key_features,
+#       additional_features: @additional_features
+      
+#     }
+#   end
+
+#   def create
+#     profile_photo
+#     Rails.logger.error("2")
+#     Rails.logger.error(@first_name)
+#     Rails.logger.error(@last_name)
+  
+#     Rails.logger.error("2")
+#     Rails.logger.error("2")
+  
+#     profile_photo_path = params[:profile_photo]
+#     if profile_photo_path.present?
+#       content_type =  @profile_photo.content_type
+#       @original_filename =  @profile_photo.original_filename
+#       @temp_file_path =  @profile_photo.tempfile.path
+  
+     
+#       save_profile_photo(profile_photo_path.tempfile)
+ 
+#       process_saved_file
+  
+#       account_details['profile_photo'] = profile_photo_path.original_filename
+#     else
+#       render json: { message: "Profile photo is required", status: false }
+#       return
+#     end
+  
+#     result = UspKodieSaveStepsJsonData.new(account_details, property_details)
+#     result_data = result.create_post_with_tags
+  
+#     Rails.logger.error("result")
+#     Rails.logger.error(result_data)
+  
+   
+#     file_url = "#{request.protocol}#{request.host_with_port}/images/#{@original_filename}"
+  
+#     render json: { message: "Data Successfully Stored", profile_photo_path: file_url, profile_photo_name: @original_filename, status: true }
+#   end
+  
+#   private
+  
+#   def save_profile_photo(file)
+#     filename = File.basename(file)
+#     local_path = Rails.root.join('public', 'images', @original_filename)
+#     @full_path = local_path
+#     FileUtils.cp(file, local_path)
+#   end
+  
+#   def process_saved_file
+   
+#     saved_file_path = Rails.root.join('public', 'images', @original_filename)
+#     @saved_file = saved_file_path
+#     Rails.logger.error("saved_file_path")
+#     Rails.logger.error(saved_file_path)
+#     File.open(saved_file_path, 'r') do |file|
+#       file_content = file.read
+#       Rails.logger.error("File content:")
+#       Rails.logger.error(file_content)
+#     end
+#   end
+  
+  
+
+#   def account_details_params
+#     params[:account_details].present? ? JSON.parse(params[:account_details]) : {}
+#   end
+
+#   def property_details_params
+#     params[:property_details].present? ? JSON.parse(params[:property_details]) : {}
+#   end
+# end
+
+
 class Api::V1::VerificationController < ApplicationController
   require 'open-uri'
   require 'fileutils'
    
   def profile_photo
+ 
+    @email = params[:email]
     @profile_photo = params[:profile_photo]
-    
+   
     Rails.logger.error("temp_file_path")
     Rails.logger.error(@temp_file_path)
     Rails.logger.error("temp_file_path2")
@@ -32,9 +171,10 @@ class Api::V1::VerificationController < ApplicationController
     Rails.logger.error(@organisation_name)
     Rails.logger.error(@referral_code)
   end
-
+ 
   def account_details
     {
+     
       user: @user,
       first_name: @first_name,
       last_name: @last_name,
@@ -45,10 +185,10 @@ class Api::V1::VerificationController < ApplicationController
       describe_yourself: @describe_yourself,
       property_manage: @property_manage,
       kodie_help: @kodie_help
-
+ 
     }
   end
-
+ 
   def property_details
     {
       location: @organisation_name,
@@ -59,57 +199,71 @@ class Api::V1::VerificationController < ApplicationController
       property_type: @property_type,
       key_features: @key_features,
       additional_features: @additional_features
-      
+     
     }
   end
-
+ 
   def create
     profile_photo
-    Rails.logger.error("2")
-    Rails.logger.error(@first_name)
-    Rails.logger.error(@last_name)
-  
-    Rails.logger.error("2")
-    Rails.logger.error("2")
-  
+    # Rails.logger.error("2")
+    # Rails.logger.error(@first_name)
+    # Rails.logger.error(@last_name)
+ 
+    # Rails.logger.error("2")
+    # Rails.logger.error("2")
+ 
+    Rails.logger.error()
+ 
     profile_photo_path = params[:profile_photo]
     if profile_photo_path.present?
       content_type =  @profile_photo.content_type
       @original_filename =  @profile_photo.original_filename
       @temp_file_path =  @profile_photo.tempfile.path
-  
+ 
      
       save_profile_photo(profile_photo_path.tempfile)
  
       process_saved_file
-  
+ 
       account_details['profile_photo'] = profile_photo_path.original_filename
     else
       render json: { message: "Profile photo is required", status: false }
       return
     end
-  
-    result = UspKodieSaveStepsJsonData.new(account_details, property_details)
+   
+    payload = {
+      email: @email,
+      exp: 1.hour.from_now.to_i
+    }
+    token=JwtService.generate_token(payload)
+    Rails.logger.error(token)
+    response.headers['Authorization'] = "Bearer #{token}"
+    result = UspKodieSaveStepsJsonData.new(account_details, property_details, @user,@email,token,1,1,)
     result_data = result.create_post_with_tags
-  
-    Rails.logger.error("result")
     Rails.logger.error(result_data)
-  
+    Rails.logger.error("result")
+ 
+ 
+    # result=UspKodieSetSignupDetailsService.new(@user,@email,token,"1","1")
+    # details= result.sp_signup_details
+    # Rails.logger.error("details-->")
+    # Rails.logger.error(details)
+ 
    
     file_url = "#{request.protocol}#{request.host_with_port}/images/#{@original_filename}"
-  
-    render json: { message: "Data Successfully Stored", profile_photo_path: file_url, profile_photo_name: @original_filename, status: true }
+   
+    render json: { message: "Data Successfully Stored",Login_details: result_data , profile_photo_path: file_url, profile_photo_name: @original_filename, status: true }
   end
-  
+ 
   private
-  
+ 
   def save_profile_photo(file)
     filename = File.basename(file)
     local_path = Rails.root.join('public', 'images', @original_filename)
     @full_path = local_path
     FileUtils.cp(file, local_path)
   end
-  
+ 
   def process_saved_file
    
     saved_file_path = Rails.root.join('public', 'images', @original_filename)
@@ -122,13 +276,13 @@ class Api::V1::VerificationController < ApplicationController
       Rails.logger.error(file_content)
     end
   end
-  
-  
-
+ 
+ 
+ 
   def account_details_params
     params[:account_details].present? ? JSON.parse(params[:account_details]) : {}
   end
-
+ 
   def property_details_params
     params[:property_details].present? ? JSON.parse(params[:property_details]) : {}
   end
--- app/services/usp_kodie_save_steps_json_data.rb
+++ app/services/usp_kodie_save_steps_json_data.rb
@@ -1,24 +1,113 @@
+# class UspKodieSaveStepsJsonData
+#   def initialize( account_details,property_details)
+#     @account_details = account_details
+#     @property_details = property_details
+  
+#   end
+
+#   def create_post_with_tags
+#     Rails.logger.error("3")
+#     Rails.logger.error(@account_details)
+#     Rails.logger.error(@property_details)
+#     connection = ActiveRecord::Base.connection.raw_connection
+#     account_details_json = connection.escape(@account_details.to_json)
+#     property_details_json = connection.escape(@property_details.to_json)
+#     Rails.logger.error(account_details_json)
+#     Rails.logger.error(property_details_json)
+#     # Assuming 'insert_data' is a column in your stored procedure
+#     result = connection.query("CALL USP_KODIE_SAVE_ACCOUNT_DETAILS('#{account_details_json}','#{property_details_json}')")
+#     connection.close
+
+#     result
+#     Rails.logger.error("4")
+#   end
+# end
+
+
+
+
+
 class UspKodieSaveStepsJsonData
-  def initialize( account_details,property_details)
+  
+  def initialize(account_details, property_details, user, email, token, device_id, device_type)
     @account_details = account_details
     @property_details = property_details
-  
+    @user = user
+    @email = email
+    @token = token
+    @device_id = device_id
+    @device_type = device_type
   end
-
+ 
   def create_post_with_tags
-    Rails.logger.error("3")
-    Rails.logger.error(@account_details)
-    Rails.logger.error(@property_details)
-    connection = ActiveRecord::Base.connection.raw_connection
-    account_details_json = connection.escape(@account_details.to_json)
-    property_details_json = connection.escape(@property_details.to_json)
-    Rails.logger.error(account_details_json)
-    Rails.logger.error(property_details_json)
-    # Assuming 'insert_data' is a column in your stored procedure
-    result = connection.query("CALL USP_KODIE_SAVE_ACCOUNT_DETAILS('#{account_details_json}','#{property_details_json}')")
-    connection.close
-
-    result
+    begin
+      Rails.logger.error("3")
+      Rails.logger.error(@account_details)
+      Rails.logger.error(@property_details)
+ 
+      Rails.logger.error(@email)
+      Rails.logger.error(@user)
+      Rails.logger.error(@token)
+      Rails.logger.error(@device_id)
+      Rails.logger.error(@device_type)
+ 
+ 
+ 
+      connection = ActiveRecord::Base.connection.raw_connection
+ 
+      account_details_json = connection.escape(@account_details.to_json)
+      property_details_json = connection.escape(@property_details.to_json)
+      Rails.logger.error(account_details_json)
+      Rails.logger.error(property_details_json)
+ 
+      # First query
+      result = connection.query("CALL USP_KODIE_SAVE_ACCOUNT_DETAILS('#{account_details_json}','#{property_details_json}')")
+ 
+      Rails.logger.error("3")
+      Rails.logger.error("user")
+      Rails.logger.error(@user)
+      Rails.logger.error("email")
+      Rails.logger.error(@email)
+      Rails.logger.error("token")
+      Rails.logger.error(@token)
+      Rails.logger.error("did")
+      Rails.logger.error(@device_id)
+      Rails.logger.error("dtype")
+      Rails.logger.error(@device_type)
+ 
+      # Check if there are more results
+      while connection.more_results?
+        # Move to the next result
+        connection.next_result
+      end
+ 
+      # Second query
+      result2 = connection.query("
+        CALL USP_KODIE_SAVE_LOGIN_DETAILS(
+          '#{@user}',
+          '#{@email}',
+          '#{@token}',
+          '#{@device_id}',
+          '#{@device_type}'
+        )
+      ")
+        Rails.logger.error(result2.to_a)
+      Rails.logger.error("Database connection closed")
+ 
+    rescue => e
+      Rails.logger.error("Error in create_post_with_tags: #{e.message}")
+      raise e # Re-raise the exception after logging
+    ensure
+      connection.close if connection
+    end
+ 
+    [result, result2]
     Rails.logger.error("4")
+    Rails.logger.error(result.to_a)
+    # Rails.logger.error(result2.to_a)
+    # result2
+    value=[user: @user, email: @email,token:  @token,device_id: @device_id,device_type: @device_type]
+    value
+   
   end
 end
